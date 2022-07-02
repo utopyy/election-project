794e83a561efb3e53df26317bc379a0d0de7b4d3
martinlej <mlej7498@gmail.com>
2022-07-02 02:35:22 +0200

    Ending SELECT details sessions

--- src/main/java/com/ipamc/election/views/components/ManageSessions.java
+++ src/main/java/com/ipamc/election/views/components/ManageSessions.java
@@ -2,20 +2,36 @@ package com.ipamc.election.views.components;
 
 import java.util.List;
 
+import org.claspina.confirmdialog.ButtonOption;
+import org.claspina.confirmdialog.ConfirmDialog;
 import org.vaadin.crudui.crud.impl.GridCrud;
 
+import com.ipamc.election.data.entity.Categorie;
+import com.ipamc.election.data.entity.Proposition;
+import com.ipamc.election.data.entity.Question;
 import com.ipamc.election.data.entity.Session;
 import com.ipamc.election.data.entity.User;
 import com.ipamc.election.services.SessionService;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.details.Details;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.ColumnTextAlign;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.dataview.GridListDataView;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.H4;
+import com.vaadin.flow.component.html.Hr;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.notification.Notification.Position;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -45,9 +61,6 @@ public class ManageSessions extends VerticalLayout {
 	private void initLayout() {
 		HorizontalLayout buttons = new HorizontalLayout();
 
-		findAllButton = new Button(VaadinIcon.REFRESH.create());
-		findAllButton.getElement().setAttribute("title", "Refresh list");
-
 		addButton = new Button(VaadinIcon.PLUS.create());
 		addButton.getElement().setAttribute("title", "Add");
 
@@ -69,8 +82,8 @@ public class ManageSessions extends VerticalLayout {
 		initDeleteButton(dataView);
 		updateButtons();
 	}
-	
-	
+
+
 
 	private void updateButtons() {
 		grid.addItemClickListener(event -> {
@@ -86,7 +99,7 @@ public class ManageSessions extends VerticalLayout {
 		grid.addComponentColumn(session -> new Button(Integer.toString(session.getUsers().size()), click -> {
 			Dialog dialog = new Dialog();
 			dialog.getElement().setAttribute("aria-label", "Add note");
-			VerticalLayout dialogLayout = dialogShowJury(dialog, session);
+			VerticalLayout dialogLayout = dialogShowJury(session);
 			dialog.add(dialogLayout);
 			dialog.setHeaderTitle(session.getName());
 			Button closeButton = new Button(new Icon("lumo", "cross"), (e) -> dialog.close());
@@ -94,13 +107,21 @@ public class ManageSessions extends VerticalLayout {
 			dialog.getHeader().add(closeButton);
 			dialog.open();
 		})).setHeader("Membres du jury");
-		grid.addColumn(new ComponentRenderer<>(Button::new, (button, session) -> {
-			button.setText(Integer.toString(session.getQuestions().size()));
+		grid.addComponentColumn(session -> new Button(Integer.toString(session.getQuestions().size()), click -> {
+			Dialog dialog = new Dialog();
+			dialog.getElement().setAttribute("aria-label", "Add note");
+			VerticalLayout dialogLayout = dialogShowQuestion(session);
+			dialog.add(dialogLayout);
+			dialog.setHeaderTitle(session.getName());
+			Button closeButton = new Button(new Icon("lumo", "cross"), (e) -> dialog.close());
+			closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+			dialog.getHeader().add(closeButton);
+			dialog.open();
 		})).setHeader("Questions");
 		grid.setItems(sessionService.findAllSessions());
 	}
-	
-	private VerticalLayout dialogShowJury(Dialog dialog, Session session) {
+
+	private VerticalLayout dialogShowJury(Session session) {
 		Grid<User> grid = setupJuryGrid();
 		GridListDataView<User> dataView = grid.setItems(session.getUsers());
 		TextField searchField = createSearchField(dataView);
@@ -113,12 +134,67 @@ public class ManageSessions extends VerticalLayout {
 		return fieldLayout;
 	}
 
+	private VerticalLayout dialogShowQuestion(Session session) {
+		VerticalLayout mainLayout = new VerticalLayout();
+		for(Question quest : session.getQuestions()) {
+			VerticalLayout detailsLayout = new VerticalLayout();
+			detailsLayout.setSpacing(false);
+			detailsLayout.setPadding(false);
+			if(!quest.getCategories().isEmpty()) {
+				for(Categorie cat : quest.getCategories()) {
+					String isRequired = "Pas obligatoire";
+					if(cat.getIsRequired()) {
+						isRequired = "Obligatoire";
+					}
+					String libelle = cat.getLibelle();
+					if(libelle.equals("Note")) {
+						libelle += " (/"+cat.getValeur()+")";
+					}
+					detailsLayout.add(new Label(libelle + " : "+ isRequired));
+				}
+			}
+			if(!quest.getPropositions().isEmpty()) {
+				String isRequired = "Pas obligatoire";
+				String isQcm = "Une seule réponse possible";
+				VerticalLayout propositions = new VerticalLayout();
+				propositions.setPadding(false);
+				detailsLayout.setSpacing(false);
+				if(quest.getMultiChoice()) {
+					isQcm = "Plusieurs réponses possibles";
+				}
+				if(quest.getPropositionRequired()) {
+					isRequired = "Obligatoire";
+				}
+				detailsLayout.add(new Label("Propositions: "+isRequired +" - "+ isQcm));
+				int cpt = 1;
+				for(Proposition prop : quest.getPropositions()) {
+					propositions.add(new Label(+cpt+": "+prop.getLibelle()));
+					cpt++;
+				}
+				Details propDetails = new Details("Liste des propositions", propositions);
+				propDetails.getStyle().set("padding-left","20px");
+				propDetails.getStyle().set("padding-top","0px");
+				detailsLayout.add(propDetails);
+			}
+			detailsLayout.setSpacing(false);
+			detailsLayout.setPadding(false);
+			detailsLayout.setAlignItems(FlexComponent.Alignment.STRETCH);
+			detailsLayout.getStyle().set("width", "450px").set("max-width", "100%");
+			mainLayout.add(new Details(quest.getIntitule(), detailsLayout));
+			mainLayout.getStyle().set("width", "450px").set("max-width", "100%");
+		}
+		return mainLayout;
+	}
+
+
+
 	private static Grid<User> setupJuryGrid() {
 		Grid<User> myGrid = new Grid<>(User.class, false);
-		myGrid.addColumn(User::getUsername).setHeader("Nom d'utilisateur").setSortable(true);;
+		myGrid.addColumn(User::getUsername).setHeader("Nom d'utilisateur").setSortable(true);
 		myGrid.addComponentColumn(userPermissions -> createPermissionIcon(userPermissions.certified())).setHeader("Certifié").setTextAlign(ColumnTextAlign.CENTER);               
 		return myGrid;
 	}
+
 
 	private static Icon createPermissionIcon(boolean isCertified) {
 		Icon icon;
@@ -161,7 +237,7 @@ public class ManageSessions extends VerticalLayout {
 		});
 		return searchField;
 	}
-	
+
 	private TextField searchBarSession(GridListDataView<Session> sessions) {
 		TextField searchField = new TextField();
 		searchField.setWidth("100%");
@@ -179,19 +255,28 @@ public class ManageSessions extends VerticalLayout {
 		});
 		return searchField;
 	}
-	
+
 	private void initDeleteButton(GridListDataView<Session> sessionsView) {
 		deleteButton.addClickListener(event ->{
 			Session sess = grid.getSelectedItems().iterator().next();
-			System.out.println("\n\n"+sess.getName()+": "+sess.getId()+"\n\n");
-			sessionService.removeSession(sess.getId());
-			sessions.remove(sess);
-			grid.deselectAll();
-			deleteButton.setEnabled(false);
-			grid.getDataProvider().refreshAll();
+			ConfirmDialog.create()
+			.withCaption("Confirmation")
+			.withMessage("Voulez vous supprimer la session "+sess.getName() +" ?")
+			.withOkButton(() -> {
+				sessionService.removeSession(sess.getId());
+				sessions.remove(sess);
+				grid.deselectAll();
+				deleteButton.setEnabled(false);
+				grid.getDataProvider().refreshAll();
+				Notification notification = Notification.show("Session supprimée!");
+				notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+				notification.setDuration(1500);
+				notification.setPosition(Position.TOP_END);
+			}, ButtonOption.focus(), ButtonOption.caption("OUI"))
+			.withCancelButton(ButtonOption.caption("NON")).open();
 		});
 	}
-	
-	
+
+
 
 }
